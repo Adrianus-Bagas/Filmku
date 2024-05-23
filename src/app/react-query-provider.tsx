47f482210db793ca6c80382ccf1227fcd02a590a
Adrianus-Bagas <bagasadrianus80@gmail.com>
2024-05-23 12:50:37 +0700

    ci: config prettier and remove devtools

--- src/app/react-query-provider.tsx
+++ src/app/react-query-provider.tsx
@@ -1,30 +1,34 @@
 "use client";
 
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {useHydrateAtoms} from "jotai/react/utils";
-import {queryClientAtom} from "jotai-tanstack-query";
-import React, {useState} from "react";
-import {Provider} from "jotai";
-import {DevTools} from "jotai-devtools";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useHydrateAtoms } from "jotai/react/utils";
+import { queryClientAtom } from "jotai-tanstack-query";
+import React, { useState } from "react";
+import { Provider } from "jotai";
 
-const HydrateAtoms = ({children, client}: {children: React.JSX.Element; client: QueryClient}) => {
+const HydrateAtoms = ({
+  children,
+  client,
+}: {
+  children: React.JSX.Element;
+  client: QueryClient;
+}) => {
   useHydrateAtoms([[queryClientAtom, client]]);
 
   return children;
 };
 
-const QueryProviders = ({children}: {children: React.JSX.Element}) => {
-  const [client] = useState(new QueryClient({defaultOptions: {queries: {staleTime: Infinity}}}));
+const QueryProviders = ({ children }: { children: React.JSX.Element }) => {
+  const [client] = useState(
+    new QueryClient({ defaultOptions: { queries: { staleTime: Infinity } } }),
+  );
 
   return (
     <>
       <QueryClientProvider client={client}>
         <Provider>
-          <DevTools />
           <HydrateAtoms client={client}>{children}</HydrateAtoms>
         </Provider>
-        <ReactQueryDevtools initialIsOpen={false} position="bottom" />
       </QueryClientProvider>
     </>
   );
