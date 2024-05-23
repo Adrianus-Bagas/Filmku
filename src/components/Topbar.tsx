2a8000517265689847c994a97dbb790b305c960f
Adrianus-Bagas <bagasadrianus80@gmail.com>
2024-05-23 14:58:53 +0700

    feat(component/topbar): add list menu and router

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,8 +1,56 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
+
+import Logo from "@/assets/images/TheMovie (1).png";
+
 export const Topbar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const menu = [
+    {
+      path: "/movies",
+      name: "Movies",
+    },
+    {
+      path: "/series",
+      name: "Series",
+    },
+    {
+      path: "/favorites",
+      name: "Favorites",
+    },
+    {
+      path: "/watchlists",
+      name: "Watchlists",
+    },
+  ];
+
   return (
     <>
-      <div className="p-3 w-full bg-white top-0 fixed z-10 hidden lg:inline-block">
-        <p className="text-black">Topbar</p>
+      <div className="w-full bg-black top-0 fixed z-10 hidden lg:inline-block">
+        <div className="flex items-center">
+          <Image
+            alt="Filmku"
+            className="w-[200px] px-3 cursor-pointer"
+            src={Logo}
+            onClick={() => router.push("/")}
+          />
+          {menu.map((i, index) => (
+            <div
+              key={index}
+              className={`${i.path.includes(pathname) && pathname !== "/" && "opacity-100"} opacity-50 bg-black p-3`}
+            >
+              <p
+                className="text-white px-3 cursor-pointer"
+                onClick={() => router.push(i.path)}
+              >
+                {i.name}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
