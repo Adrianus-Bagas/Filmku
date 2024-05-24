c50472216c4b50bb46be5a60f9e7943b465d294f
Adrianus-Bagas <bagasadrianus80@gmail.com>
2024-05-24 17:37:53 +0700

    feat(component): add bottombar component

--- src/components/Bottombar.tsx
+++ src/components/Bottombar.tsx
@@ -1,8 +1,63 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+
+import {
+  FavoritesIcon,
+  FilmIcon,
+  RatedIcon,
+  SeriesIcon,
+  WatchlistsIcon,
+} from "@/assets/icons";
+import { menu } from "@/utils/constants";
+
 export const Bottombar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <>
-      <div className="p-3 w-full bg-white fixed bottom-0 z-10 lg:hidden inline-block">
-        <p className="text-black">Bottombar</p>
+      <div className="w-full h-[56px] bg-black fixed bottom-0 z-10 md:hidden inline-block py-2">
+        <div className="flex justify-evenly items-center">
+          {menu.map((i, index) => (
+            <div
+              key={index}
+              className={`${i.path === pathname && pathname !== "/" ? "opacity-100" : "opacity-50"} bg-black p-1 cursor-pointer`}
+            >
+              {i.name === "Movies" && (
+                <div className="flex justify-center">
+                  <FilmIcon className="w-6 h-6" />
+                </div>
+              )}
+              {i.name === "Series" && (
+                <div className="flex justify-center">
+                  <SeriesIcon className="w-6 h-6 flex justify-center" />
+                </div>
+              )}
+              {i.name === "Favorites" && (
+                <div className="flex justify-center">
+                  <FavoritesIcon className="w-6 h-6 flex justify-center" />
+                </div>
+              )}
+              {i.name === "Watchlists" && (
+                <div className="flex justify-center">
+                  <WatchlistsIcon className="w-6 h-6 flex justify-center" />
+                </div>
+              )}
+              {i.name === "Rated" && (
+                <div className="flex justify-center">
+                  <RatedIcon className="w-6 h-6 flex justify-center" />
+                </div>
+              )}
+              <div
+                className="text-white px-1 text-[10px]"
+                onClick={() => router.push(i.path)}
+              >
+                {i.name}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const menu = [
+  {
+    path: "/movies",
+    name: "Movies",
+  },
+  {
+    path: "/series",
+    name: "Series",
+  },
+  {
+    path: "/favorites",
+    name: "Favorites",
+  },
+  {
+    path: "/watchlists",
+    name: "Watchlists",
+  },
+  {
+    path: "/rated",
+    name: "Rated",
+  },
+];
